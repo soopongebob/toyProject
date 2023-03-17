fa7c17cedcd69de2a187f8e7adbce0fff52f7b3f
soopongebob <92519479+soopongebob@users.noreply.github.com>
2023-03-17 22:18:13 +0900

    Revert "init"
    This reverts commit 879b64341737c2cd365795ff1a11a5df58badf45.

--- src/main/java/toyproject/community/domain/Category.java
+++
@@ -1,5 +0,0 @@
-package toyproject.community.domain;
-
-public enum Category {
-    QNA, DAILY, COLUMN, KNOWLEDGE, STUDY
-}
--- src/main/resources/static/js/etc.js
+++
@@ -1,8 +0,0 @@
-function onOver(){
-    var ss = document.getElementById("subject");
-    ss.style.color = "skyblue";
-}
-function onOut(){
-    var ss = document.getElementById("subject");
-    ss.style.color = "black";
-}
